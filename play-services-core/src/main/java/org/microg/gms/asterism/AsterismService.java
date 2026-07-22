56e4dc80b5fd1cdb64793d12c6458f6ea9bbfbfa
Ahavahdev1 <brunodesidera@mail.com>
2026-07-22 17:32:24 -0300

    feat(rcs): implement clean baseline Asterism and Constellation stubs

---
+++ play-services-core/src/main/java/org/microg/gms/asterism/AsterismService.java
@@ -0,0 +1,18 @@
+﻿package org.microg.gms.asterism;
+
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.IInterface;
+import android.util.Log;
+import org.microg.gms.BaseService;
+import org.microg.gms.common.GmsService;
+
+public class AsterismService extends BaseService {
+    private static final String TAG = "GmsAsterism";
+    public AsterismService() { super(TAG, GmsService.ASTERISM); }
+    @Override public IBinder onBind(android.content.Intent intent) { return new AsterismBinder(); }
+    private static class AsterismBinder extends Binder implements IInterface {
+        @Override public IBinder asBinder() { return this; }
+        public boolean bypass() { return true; }
+    }
+}
---
+++ play-services-core/src/main/java/org/microg/gms/constellation/ConstellationService.java
@@ -0,0 +1,18 @@
+﻿package org.microg.gms.constellation;
+
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.IInterface;
+import android.util.Log;
+import org.microg.gms.BaseService;
+import org.microg.gms.common.GmsService;
+
+public class ConstellationService extends BaseService {
+    private static final String TAG = "GmsConstellation";
+    public ConstellationService() { super(TAG, GmsService.CONSTELLATION); }
+    @Override public IBinder onBind(android.content.Intent intent) { return new ConstellationBinder(); }
+    private static class ConstellationBinder extends Binder implements IInterface {
+        @Override public IBinder asBinder() { return this; }
+        public String attest() { return "valid"; }
+    }
+}
